a379c3f6d690b6aa270e3e329f8a3cb6e51c1339
lerry <laijun@bookuu.com>
2017-05-18 13:47:20 +0800

    完善插入排序代码

--- src/algorithm/sort/Insertion.java
+++ src/algorithm/sort/Insertion.java
@@ -6,4 +6,36 @@ package algorithm.sort;
  */
 public class Insertion {
 
+    /**
+     * 举例子解释比较容易理解
+     * 拿int类型数组举例
+     * int a =  [5,3,2,10,4,11,23]
+     * 遍历数组a,索引记为index,初始为0
+     * 当index为0时，没软用。index++
+     * 当index =1 时，j>0，调用less()函数比较两值大小less(3,5)返回true，交换两个元素位置j--,跳出。此时a = [3,5,2,10,4,11,23];
+     * index = 2, j = 2,j >0,less(2,5)返回true，交换位置,a = [3,2,5,10,4,11,23],j--,j = 1,less(2,3)返回true，交换位置
+     * a = [2,3,5,10,4,11,23],下面的套路应该都懂了，不写啦。
+     */
+
+    public static void sort(Comparable[] comparables) {
+        //将a[]按升序排列
+        int length = comparables.length;
+
+        for (int i = 0; i < length; i++) {
+            for (int j = i; j > 0 && less(comparables[j], comparables[j - 1]); j--) {
+                exchange(comparables, j, j - 1);
+            }
+        }
+    }
+
+    private static boolean less(Comparable comparable, Comparable comparable1) {
+        return comparable.compareTo(comparable1) < 0;
+    }
+
+    //交换
+    private static void exchange(Comparable[] comparables, int i, int j) {
+        Comparable t = comparables[i];
+        comparables[i] = comparables[j];
+        comparables[j] = t;
+    }
 }
